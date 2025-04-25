fe54ca4c94260dcbf651218c05153917c4effa92
Lokendra Dhote <137758094+Lokendram10@users.noreply.github.com>
2025-04-25 14:15:31 +0530

    Update  Final commit monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -7,6 +7,6 @@ RAM_USAGE=$(free | awk '/Mem/ { printf("%.2f"), $3/$2 * 100.0 }')
 CPU_USAGE=$(mpstat 1 1 | grep "Average" | awk '{ print 100 - $12 }')
 
 # Send a Telegram alert with RAM and CPU usage
-curl -s -X POST https://api.telegram.org/bot7805971663:AAF1N942lWTWZ7qB6uJcw94-uX4k4wdNXIA/sendMessage \
-    -d chat_id=1405067651 \
+curl -s -X POST https://api.telegram.org/botYourBotApi/sendMessage \
+    -d chat_id=YourChatId \
     -d text="Current RAM Usage: ${RAM_USAGE}% | Current CPU Usage: ${CPU_USAGE}%"
